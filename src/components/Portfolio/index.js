9648fbefd1d80d0e15ea739d20836cbfff26dbaf
AHamdy <abdulla.mh@hotmail.com>
2022-10-28 13:41:37 -0400

    added project screenshots

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -46,7 +46,7 @@ function Portfolio () {
                 {projects.map((project) => (
                         <div className="card" key={project.projectName} style={{width: "18rem"}}>
                             <img
-                                src={require(`../../assets/${category}/${i}.jpg`).default}
+                                src={require(`../../assets/${project.pngName}.png`).default}
                                 class="card-img-top"></img>
                             <div className="card-body">
                             <h5 className="card-title">{project.projectName}</h5>
